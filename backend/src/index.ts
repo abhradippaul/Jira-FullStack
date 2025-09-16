9bd345c4bde5202e8274582f0058a6f895d00421
Abhradip Paul <abhradipserampore@gmail.com>
2025-09-16 17:29:49 +0530

    Frontend and backend setup completed for jira.

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import cors from "cors";
+import express, { urlencoded } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  res.json({ msg: "hello" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server connected successfully on port no", PORT);
+});
